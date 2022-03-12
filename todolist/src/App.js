efa1d17d4d8deed9e9cd01b5010910d962a33118
aresgowar <ntd31101999@gmail.com>
2022-03-12 23:52:23 +0700

    todolist1

--- todolist/src/App.js
+++ todolist/src/App.js
@@ -1,25 +1,58 @@
-import logo from './logo.svg';
 import './App.css';
+import React from 'react';
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+class App extends React.Component {
+  constructor(props) {
+    super(props)
+    this.state = { taskName: '',  task: []  }
+  }
+
+  myTaskChangeHandler = (event) => {
+    this.setState({ taskName: event.target.value })
+    //console.log('myTaskChangeHandler', this.state.taskName);
+  }
+
+  addTask = (event) => {
+    console.log('addTask', this.state.taskName);
+    if (this.state.taskName === '') {
+      return
+    }
+    this.state.task.push(this.state.taskName)
+    this.setState({ taskName: ''})
+    //this.setState({ task: this.state.taskName})
+  }
+
+  render() {
+    return (
+      <div className="App">
+          <br />
+          To Do List
+          <br />
+          <div className="aligned">
+            <img
+              src='.\assest\iconfinder_gnome-app-install_29240 (1).png'
+              alt='Add Task'
+              width='25'
+              style={{ cursor: 'pointer' }}
+              title='Bấm để thêm task'
+              onClick={() => this.addTask()}
+            />
+            <input
+              type='text'
+              value={this.state.taskName}
+              onChange={this.myTaskChangeHandler}
+            />
+            
+          </div>
+          <br />
+            <ul>
+              {this.state.task.map((value, index) => {
+                return <li key={index} className='taskItem'>{value}</li>
+              })}
+            </ul>
+      </div>
+    );
+  }
 }
 
 export default App;
